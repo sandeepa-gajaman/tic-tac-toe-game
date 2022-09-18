abfcd8baaf8162bf6e880be28f9682227d581507
sgajaman <sgajaman@wiley.com>
2022-09-18 11:07:12 +0530

    complete lifting-state-up

--- src/index.js
+++ src/index.js
@@ -2,25 +2,67 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        value: null,
-      }
-    }
+  class Square extends React.Component {
+
     render() {
+
+      console.log('Square’s render method');
+
+      /*
+      1.The onClick prop on the built-in DOM <button> component tells React to set up a click event listener.
+      2.When the button is clicked, React will call the onClick event handler that is defined in Square’s render() method.
+      3.This event handler calls this.props.onClick(). The Square’s onClick prop was specified by the Board.
+      **/
+
       return (
-        <button className="square" onClick={() => this.setState({value: 'X'})}>
-          {this.state.value}
+        <button
+          className="square"
+          onClick={() => this.props.onClick()}
+        >
+          {this.props.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+
+    /*
+    To collect data from multiple children, or to have two child components communicate with each other, 
+    you need to declare the shared state in their parent component instead. The parent component can pass 
+    the state back down to the children by using props; this keeps the child components in sync with each 
+    other and with the parent component.
+    **/
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
+
+    handleClick(i) {
+      console.log('The Square calls the Board’s handleClick when clicked.');
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+    
     renderSquare(i) {
-      return <Square value={i} />;
+
+      console.log('renderSquare fucntion in Board');
+
+      /*
+      Passing down two props from Board to Square: "value" and "onClick". 
+      The onClick prop is a function that Square can call when clicked.
+      **/
+
+      return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );  
     }
   
     render() {
